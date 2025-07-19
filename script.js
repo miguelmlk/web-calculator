bff37a59dbf1ce8d82003efdbafde16787ab29d1
Miguel Malek <miguel.mlk73@gmail.com>
2025-07-19 21:10:22 +0200

    FEAT(script.js) -> output is now displaying the number button clicked

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const output = document.getElementById("output");
-
+const output = document.querySelector("#output");
+document.querySelectorAll(".digit").forEach(button => {
+  button.addEventListener("click", () => {
+    const value = button.getAttribute("data-value");
+    if (output.innerHTML === "0" && value === "0") {
+        output.innerHTML = "0";
+    } else if (output.innerHTML === "0") {
+        output.innerHTML = value;
+    } else {
+        output.innerHTML += value;
+    }
+})});
